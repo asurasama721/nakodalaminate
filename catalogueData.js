492612ae66cc5209e97cf40c3ed90b41c7ed368a
asurasama721 <164638690+asurasama721@users.noreply.github.com>
2025-08-03 07:06:10 -0700

    files added

---
+++ catalogueData.js
@@ -0,0 +1,33 @@
+const catalogData = [
+  {
+        link: "https://drive.google.com/file/d/12qIbGnrUrif0CO3-mVRjUXCghxxdVrol/view?usp=drive_link",
+        imageSrc: "frontpage/megalam.png",
+        altText: "MEGALAM Cover",
+        category: "Megalam",
+      },  
+        {
+        link: "https://drive.google.com/file/d/1dOS4tcqaXtgTFm0SkeWGZEWQoGVoIbTO/view?usp=drive_link",
+        imageSrc: "frontpage/glimmer.png",
+        altText: "GLIMMER ACRYLIC Cover",
+        category: "Glimmer Acrylic",
+      },    
+      {
+        link: "https://drive.google.com/file/d/1DIBtTo8pjaHFY8raFis53892Ey1DPJAj/view?usp=drive_link",
+        imageSrc: "frontpage/movado.png",
+        altText: "MOVADO LOUVERS Cover",
+        category: "Movado Louvers",
+      },  
+      {
+        link: "https://drive.google.com/file/d/1zLUfnGmo5QHFu0D5fIRvYWRUASlJReag/view?usp=drive_link",
+        imageSrc: "frontpage/treeform.png",
+        altText: "TREEFORM Cover",
+        category: "Tree Form",
+      }, 
+      {
+        link: "https://drive.google.com/file/d/1PXe2Bc50md0zxhfNS8JotN_5ANd2wTWR/view?usp=drive_link",
+        imageSrc: "frontpage/supermica.png",
+        altText: "SUPERMICA Cover",
+        category: "Super Mica",
+      },  
+       
+    ];
